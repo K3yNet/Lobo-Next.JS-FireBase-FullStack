3096c22186de6d697b4988c13270708e64981632
K3yNet <enzo_vel@hotmail.com>
2023-07-18 12:20:15 -0300

    Update ProfilePage.tsx

--- Código/pages/ProfilePage.tsx
+++ Código/pages/ProfilePage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, query, where, getDocs, deleteDoc, doc, setDoc } from 'firebase/firestore';
 import { useProfileController as useUserController } from '../controllers/ProfileController';
 import { useProfileController as useBusinessController } from '../controllers/ProfileBusinessController';
 
@@ -7,6 +7,8 @@ function ProfilePage() {
   const { isLoggedIn, userData: user } = useUserController();
   const { userData: business } = useBusinessController();
   const [pets, setPets] = useState([]);
+  const [editingPet, setEditingPet] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchPets = async () => {
@@ -14,7 +16,7 @@ function ProfilePage() {
       const petsRef = collection(firestore, 'pets');
       const q = query(petsRef, where('userId', '==', business?.uid));
       const querySnapshot = await getDocs(q);
-      const petList = querySnapshot.docs.map((doc) => doc.data());
+      const petList = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
       setPets(petList);
     };
 
@@ -22,6 +24,44 @@ function ProfilePage() {
       fetchPets();
     }
   }, [business]);
+
+  const handleEditPet = (pet) => {
+    setEditingPet(pet);
+    setShowModal(true);
+  };
+
+  const handleDeletePet = async (petId) => {
+    try {
+      const firestore = getFirestore();
+      const petDocRef = doc(firestore, 'pets', petId);
+      await deleteDoc(petDocRef);
+      console.log('Pet excluído com sucesso!');
+      // Atualize a lista de pets para remover o pet excluído
+      setPets(pets.filter((pet) => pet.id !== petId));
+    } catch (error) {
+      console.log('Erro ao excluir o pet:', error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const firestore = getFirestore();
+      const petDocRef = doc(firestore, 'pets', editingPet.id);
+      await setDoc(petDocRef, editingPet);
+      console.log('Dados do pet atualizados com sucesso!');
+      setEditingPet(null);
+      setShowModal(false);
+    } catch (error) {
+      console.log('Erro ao atualizar dados do pet:', error);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setEditingPet(null);
+    setShowModal(false);
+  };
 
   if (!isLoggedIn) {
     return <h1 className="text-center">Você não está logado</h1>;
@@ -153,9 +193,9 @@ function ProfilePage() {
                                         <p className="card-text mb-2"><strong>Idade:</strong> {pet.idadePet} Anos</p>
                                         <p className="card-text mb-2"><strong>Peso:</strong> {pet.pesoPet} Kg</p>
                                         <p className="card-text mb-2"><strong>Raça:</strong> {pet.racePet}</p>
-                                        <a href="#" className="btn btn-outline-dark">
+                                        <button type="button" className="btn btn-outline-dark" onClick={() => handleEditPet(pet)}>
                                           Editar Informações do Pet <i className="material-icons-outlined"></i>
-                                        </a>
+                                        </button>
                                       </div>
                                     </div>
                                   </div>
@@ -202,6 +242,96 @@ function ProfilePage() {
             )}
           </div>
         </div>
+        {showModal && (
+          <div className="modal modal-sheet position-fixed d-block bg-body-secondary p-4">
+            <div className="modal-dialog modal-dialog-centered" role="document">
+              <div className="modal-content rounded-3 shadow border border-dark">
+                <div className="modal-overlay">
+                  <div className="modal-content">
+                    <h1 className="fw-bold mb-4 fs-4">Editar Informações do Pet</h1>
+                    <form onSubmit={handleSubmit}>
+                      <div className="mb-3">
+                        <label htmlFor="petName" className="form-label">Nome do Pet:</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="petName"
+                          name="petName"
+                          value={editingPet.nomePet}
+                          onChange={(e) => setEditingPet({ ...editingPet, nomePet: e.target.value })}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="especiePet" className="form-label">Espécie do Pet:</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="especiePet"
+                          name="especiePet"
+                          value={editingPet.especiePet}
+                          onChange={(e) => setEditingPet({ ...editingPet, especiePet: e.target.value })}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="petRace" className="form-label">Raça do Pet:</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="petRace"
+                          name="petRace"
+                          value={editingPet.racePet}
+                          onChange={(e) => setEditingPet({ ...editingPet, racePet: e.target.value })}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="idadePet" className="form-label">Idade do Pet:</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="idadePet"
+                          name="idadePet"
+                          value={editingPet.idadePet}
+                          onChange={(e) => setEditingPet({ ...editingPet, idadePet: e.target.value })}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="petPeso" className="form-label">Peso do Pet:</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="petPeso"
+                          name="petPeso"
+                          value={editingPet.pesoPet}
+                          onChange={(e) => setEditingPet({ ...editingPet, pesoPet: e.target.value })}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="businessName" className="form-label">Nome da Instituição Responsável:</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="businessName"
+                          name="businessName"
+                          value={editingPet.businessName}
+                          onChange={(e) => setEditingPet({ ...editingPet, businessName: e.target.value })}
+                        />
+                      </div>
+                      <div className="d-flex justify-content-center">
+                        <button type="submit" className="btn btn-primary me-2">Salvar</button>
+                        <button type="button" className="btn btn-danger me-2" onClick={() => handleDeletePet(editingPet.id)}>
+                          Excluir Pet
+                        </button>
+                        <button type="button" className="btn btn-secondary" onClick={handleCloseModal}>
+                          Fechar
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </>
     );
   }
